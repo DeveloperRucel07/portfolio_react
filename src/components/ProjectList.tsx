bef824e59b0e73383c885787cff42739e2cade5e
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-11-23 13:06:43 +0100

    add Project and render them

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,15 +1,61 @@
-import ProjectDetail from "./subcomponents/ProjectDetail"
+import ProjectDetail from "./subcomponents/ProjectDetail";
+import project_join from '../assets/images/project_join.png';
+import project_sharkie from '../assets/images/project_sharkie.png';
+
+export type Project ={
+  title:string,
+  technologie: string[],
+  about:string,
+  imageUrl:string;
+  gitGubLink:string,
+  projectLink:string
+}
 
 const ProjectList = () => {
+  const projectLists:Project[]= [
+   {
+      title: "Join",
+      technologie: ["JavaScript", "Firebase", "HTML", "CSS"],
+      about:
+        "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      imageUrl: project_join,
+      gitGubLink: "https://github.com/DeveloperRucel07/Join",
+      projectLink: "https://join.rucel-tsafack.com/index.html",
+    },
+    {
+      title: "Sharkie",
+      technologie: ["OOP", "JavaScript", "HTML", 'CSS'],
+      about:
+        "A simple jump-and-run game based on object-oriented programming. Help Sharkie find the coins and poison bottles to fight the killer whale and protect the ocean.",
+      imageUrl: project_sharkie,
+      gitGubLink: "https://github.com/DeveloperRucel07/sharkie",
+      projectLink: "https://sharkie.rucel-tsafack.com/index.html",
+    },
+    {
+      title: "ShopDev",
+      technologie: ["Angular", "TypeScript", "Tailwind CSS", "Material Design", "SCSS"],
+      about:
+        "A complete e-commerce store with authentication, real-time database, and secure payment integration.",
+      imageUrl: project_join,
+      gitGubLink: "https://github.com/DeveloperRucel07/shopdev",
+      projectLink: "https://shopdev.rucel-tsafack.com/index.html",
+    },
+
+  ]
+
+
   return (
     <section className="flex flex-col items-center justify-center w-full gap-10 pt-20 md:pt-20 md:pb-10 mb-20 md:mt-20 mr-2 ml-2" id="projects">
         <h3 className="font-bold text-5xl md:text-6xl lg:text-8xl text-secondary mt-5 mb-5 text-center">
             My Projects
         </h3>
         <div className="flex flex-col w-full gap-12 p-2">
-            <ProjectDetail></ProjectDetail>
-            <ProjectDetail></ProjectDetail>
-            <ProjectDetail></ProjectDetail>
+          {projectLists.map((project, index) =>(
+            <ProjectDetail
+            key={index}
+            index={index} 
+            project={project}></ProjectDetail>
+          ))}
         </div>
     </section>
   )
--- src/components/subcomponents/ProjectDetail.tsx
+++ src/components/subcomponents/ProjectDetail.tsx
@@ -1,33 +1,43 @@
-import project_join from '../../assets/images/project_join.png';
+import type { Project } from '../ProjectList';
 
-const ProjectDetail = () => {
+type ProjectDetailProps = {
+    index: number,
+    project: Project
+}
+
+const ProjectDetail = ( {index, project}: ProjectDetailProps) => {
+    function isEven(n: number): boolean {
+        return n % 2 === 0;
+    }
+
   return (
-    <div className="flex flex-col-reverse md:flex-row items-center justify-center w-full md:h-80 gap-6">
+    <div className={`flex flex-col-reverse ${isEven(index) ? 'md:flex-row': 'md:flex-row-reverse'}  items-center justify-center w-full md:h-80 gap-6`}>
         <div className="flex items-center flex-col justify-center h-full md:w-[50%]">
-            <img className='object-cover' src={project_join} alt="image project" />
+            <img className='object-cover h-full w-full rounded-md md:rounded-none' src={project.imageUrl} alt="image project" />
             <div className='flex flex-row items-center justify-evenly w-full pt-4 md:hidden'>
-                <a href='#contact' className='flex items-center justify-center w-25 h-12 border-2 border-secondary text-center bg-secondary text-md font-bold' >
+                <a href={project.gitGubLink} className='flex items-center justify-center w-25 h-12 border-2 border-secondary text-center bg-secondary text-md font-bold rounded-xl' >
                     GitHub
                 </a>
 
-                <a href='#contact' className='flex items-center justify-center w-30 h-12 border-2 border-primary text-center bg-primary text-md font-bold text-bg-color' >
+                <a href={project.projectLink} className='flex items-center justify-center w-30 h-12 border-2 border-primary text-center bg-primary text-md font-bold text-bg-color rounded-xl' >
                     Let's Talk
                 </a> 
             </div>
         </div>
         <div className="flex flex-col items-center justify-center h-full text-primary pt-2 pb-3">
-            <h4 className='font-bold text-3xl md:text-5xl text-primary mt-5 mb-5 w-50 text-center'>Join</h4>
-            <h5 className='text-center w-full pt-2 pb-3 text-xl'>JavaScript | Firebase | HTML | CSS </h5>
+            <h4 className='font-bold text-3xl md:text-5xl text-primary mt-5 mb-5 w-50 text-center'>{project.title}</h4>
+            <h5 className='text-center w-full pt-2 pb-3 text-xl'>
+                {project.technologie.join(" | ")}
+            </h5>
             <p className='text-center w-full'>
-                Task manager inspired by the Kanban System. 
-                Create and organize tasks using drag and drop functions, assign users and categories. 
+                {project.about}
             </p>
             <div className='hidden md:flex flex-row items-center justify-evenly w-full p-6'>
-                <a href='#contact' className='flex items-center justify-center w-[175px] h-12 border-2 border-secondary text-center bg-secondary text-md font-bold' >
+                <a href={project.gitGubLink} className='flex items-center justify-center w-[175px] h-12 border-2 border-secondary text-center bg-secondary text-md font-bold' >
                     GitHub
                 </a>
 
-                <a href='#contact' className='flex items-center justify-center w-[175px] h-12 border-2 border-primary text-center bg-primary text-md font-bold text-bg-color' >
+                <a href={project.projectLink} className='flex items-center justify-center w-[175px] h-12 border-2 border-primary text-center bg-primary text-md font-bold text-bg-color' >
                     Live Test
                 </a> 
             </div>
