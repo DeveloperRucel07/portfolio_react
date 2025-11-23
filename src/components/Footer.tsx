918ad7a6b37a51436657f145997e9bc6790db95f
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-11-23 09:35:45 +0100

    update Teamplayer comopnent for horizontal scroll

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import SocialMediaLink from './subcomponents/SocialMediaLink'
 
 const Footer = () => {
   return (
-    <footer className='flex justify-between items-center w-full p-2 flex-col-reverse md:flex-row'>
+    <footer className='flex justify-between items-center w-full p-2 flex-col-reverse md:flex-row shadow-[0_-4px_8px_rgba(0,0,0,0.12)]'>
       <section className="flex sm:justify-between items-center w-full md:w-[70%] flex-col sm:flex-row gap-6 p-4">
         <div className='flex flex-col items-center justify-center'>
           <img src={logo_primary} alt="" className='w-15 h-6 sm:w-20 sm:h-10' />
